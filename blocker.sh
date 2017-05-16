dc5c8d660d81766a1dbfcf0dd510af00b16db5bd
Charlie Wood <charliemowood@gmail.com>
2017-05-16 21:01:39 +0100

    Comment out code

--- blocker.sh
+++ blocker.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-sudo sh -c "echo '127.0.0.1 www.reddit.com' >> /etc/hosts"
-sudo sh -c "echo '127.0.0.1 reddit.com' >> /etc/hosts"
-sudo sh -c "echo '::1 www.reddit.com' >> /etc/hosts"
-sudo sh -c "echo '::1 reddit.com' >> /etc/hosts"
+# Remove # symbol to uncommment and run the code.
+# The next four lines would allow you to block site example.com
+
+# sudo sh -c "echo '127.0.0.1 www.example.com' >> /etc/hosts"
+# sudo sh -c "echo '127.0.0.1 example.com' >> /etc/hosts"
+# sudo sh -c "echo '::1 www.example.com' >> /etc/hosts"
+# sudo sh -c "echo '::1 example.com' >> /etc/hosts"
 
 # Add additional sites by copying and pasting the commented out code
-# Change # for whatever site you want to block
+# Change * for whatever site you want to block.
 
-# sudo sh -c "echo '127.0.0.1 www.#.com' >> /etc/hosts"
-# sudo sh -c "echo '127.0.0.1 #.com' >> /etc/hosts"
-# sudo sh -c "echo '::1 www.#.com' >> /etc/hosts"
-# sudo sh -c "echo '::1 #.com' >> /etc/hosts"
+# sudo sh -c "echo '127.0.0.1 www.*.com' >> /etc/hosts"
+# sudo sh -c "echo '127.0.0.1 *.com' >> /etc/hosts"
+# sudo sh -c "echo '::1 www.*.com' >> /etc/hosts"
+# sudo sh -c "echo '::1 *.com' >> /etc/hosts"
